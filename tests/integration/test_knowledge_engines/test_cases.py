880bb15ad1efefc3fd407c18fe3f46a5552af243
marcin p. joachimiak <4625870+realmarcin@users.noreply.github.com>
2023-03-21 19:08:30 -0700

    grouped Go-CAM tests

--- tests/integration/test_knowledge_engines/test_cases.py
+++ tests/integration/test_knowledge_engines/test_cases.py
@@ -20,6 +20,7 @@ CASES = [
     ("reaction.GeneToReaction", "reaction-21290071"),
     ("gocam.GoCamAnnotations", "gocam-27929086"),
     ("gocam.GoCamAnnotations", "gocam-33246504"),
+    ("gocam.GoCamAnnotations", "gocam-betacat"),
     ("environmental_sample.Study", "environmental-sample-hyporheic"),
     ("treatment.DiseaseTreatmentSummary", "treatment-marfan"),
     ("treatment.DiseaseTreatmentSummary", "treatment-eds"),
@@ -27,7 +28,7 @@ CASES = [
     ("mendelian_disease.MendelianDisease", "mendelian-disease-marfan"),
     ("environmental_sample.Study", "amphibian_skin"),
     ("environmental_sample.Study", "human_urban_green_space"),
-    ("environmental_sample.Study", "human_smoking_China"),
+    ("environmental_sample.Study", "human_smoking_China")
 ]
 
 
