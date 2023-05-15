f783045d6d6e7675c1d38e39a018d9b15c21ff33
cmungall <cjm@berkeleybop.org>
2023-05-14 18:03:03 -0700

    lint

--- src/ontogpt/streamlit/spindoctor.py
+++ src/ontogpt/streamlit/spindoctor.py
@@ -6,13 +6,17 @@ from oaklib import get_adapter
 
 from ontogpt.engines import create_engine
 from ontogpt.engines.enrichment import EnrichmentEngine, GeneDescriptionSource
-from ontogpt.engines.knowledge_engine import MODEL_GPT_4, MODEL_GPT_3_5_TURBO, MODEL_TEXT_DAVINCI_003
+from ontogpt.engines.knowledge_engine import (
+    MODEL_GPT_3_5_TURBO,
+    MODEL_GPT_4,
+    MODEL_TEXT_DAVINCI_003,
+)
 from ontogpt.utils.gene_set_utils import GeneSet
 
 go = get_adapter("sqlite:obo:go")
 
 # Title of the app
-st.title('SPINDOCTOR')
+st.title("SPINDOCTOR")
 st.caption("A tool for summarizing gene sets using GPT")
 
 col1, col2 = st.columns(2)
@@ -21,21 +25,21 @@ col1, col2 = st.columns(2)
 gene_symbols = col1.text_area("Enter a list of human gene symbols")
 
 model = col1.selectbox(
-    'Select the model:',
-    (MODEL_GPT_3_5_TURBO, MODEL_TEXT_DAVINCI_003, MODEL_GPT_4)
+    "Select the model:", (MODEL_GPT_3_5_TURBO, MODEL_TEXT_DAVINCI_003, MODEL_GPT_4)
 )
 
 source = col1.selectbox(
-    'Select the gene description source:',
-    (GeneDescriptionSource.ONTOLOGICAL_SYNOPSIS.value,
-     GeneDescriptionSource.NARRATIVE_SYNOPSIS.value,
-     GeneDescriptionSource.NONE.value,
-     )
+    "Select the gene description source:",
+    (
+        GeneDescriptionSource.ONTOLOGICAL_SYNOPSIS.value,
+        GeneDescriptionSource.NARRATIVE_SYNOPSIS.value,
+        GeneDescriptionSource.NONE.value,
+    ),
 )
 
 
 # Button for parsing and displaying the names
-if col1.button('Summarize genes'):
+if col1.button("Summarize genes"):
     gene_symbols = [symbol.strip() for symbol in re.split(r"[\-,;\s]+", gene_symbols)]
     gene_set = GeneSet(name="TEMP", gene_symbols=gene_symbols)
     ke = create_engine(None, EnrichmentEngine, model=model)
@@ -46,8 +50,8 @@ if col1.button('Summarize genes'):
     col1.header("Genes")
     for gene_id in gene_set.gene_ids:
         col1.write(f" * {gene_id}")
-    #st.write("## Term Strings")
-    #for term_string in results.term_strings:
+    # st.write("## Term Strings")
+    # for term_string in results.term_strings:
     #    st.write(f" * {term_string}")
     col2.write("## Terms")
     for term_id in results.term_ids:
@@ -58,6 +62,3 @@ if col1.button('Summarize genes'):
             col2.markdown(f" * UNPARSED {term_id}")
     col2.header("Summary")
     col2.caption(results.summary)
-
-
-
