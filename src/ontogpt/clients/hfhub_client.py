59ac3fa23099966bcfd50a917a971d3d0c6d5a32
caufieldjh <j.harry.caufield@gmail.com>
2023-06-26 13:26:41 -0400

    Retrieve model from hub

--- src/ontogpt/clients/hfhub_client.py
+++ src/ontogpt/clients/hfhub_client.py
@@ -1,19 +1,33 @@
 """HuggingFace Hub Client."""
 import logging
+import os
+
 from dataclasses import dataclass
-from typing import List, Union
-from urllib import parse
-
-import requests
 from oaklib.utilities.apikey_manager import get_apikey_value
+from langchain import HuggingFaceHub
 
+# Note: See https://huggingface.co/models?pipeline_tag=text-generation&sort=downloads
+# for all relevant models
 
 @dataclass
 class HFHubClient:
     """A client for the HuggingFace Hub API."""
 
     try:
-        ncbi_key = get_apikey_value("hfhub-key")
+        api_key = get_apikey_value("hfhub-key")
     except ValueError:
-        ncbi_key = None
         logging.error("HuggingFace Hub API key not found. See README.")
+
+    def get_model(self, modelname: str) -> HuggingFaceHub:
+        """Retreive a model from the Hub, given its repository name.
+
+        Returns a model object of type
+        langchain.llms.huggingface_hub.HuggingFaceHub
+        """
+
+        model = HuggingFaceHub(repo_id=modelname,
+                               model_kwargs={"temperature": 0, "max_length": 64},
+                               huggingfacehub_api_token=self.api_key)
+
+        return model
+
