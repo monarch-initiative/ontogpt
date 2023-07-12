7fcaf0f96fbffcb6502b04ed96c5b6bb57f7b2e1
AgranyaGitHub <askagranya1064@gmail.com>
2023-07-12 11:43:24 -0400

    Made changes to filter out null values

--- output_parser.py
+++ output_parser.py
@@ -3,10 +3,22 @@ from src.ontogpt.io.csv_wrapper import write_obj_as_csv
 import time
 import pprint
 
+NULL_VALS = ['', 'Not mentioned', 'none mentioned', 'Not mentioned in the text', 
+             'Not mentioned in the provided text.', 'No exposures mentioned in the text.', 
+             'None', 'N/A', 'No exposures mentioned in the text.', 'None mentioned in the text', 
+             'Not mentioned in the text.', 'None relevant', 'None mentioned in the text.', 
+             'No gene to molecular activity relationships mentioned in the text.',
+             'No genes mentioned', 'No genes mentioned in the text.', ]
 lines = []
+def triple_print(dict):
+    key = next(iter(dict))
+    for i in range(len(dict[key])):
+        curr = []
+        for value in dict.values():
+            curr.append(value[i])
+        print(curr)
 
 output_file = "long_output.yaml"#"output.yaml"
-
 with open(output_file, "r") as file:
     to_print = False
     for line in file:
@@ -31,14 +43,15 @@ for i in range(len(cleaned_lines) - 1):
         cleaned_lines.insert(i+3, "  gene_exposures_relationships:")
         i += 4
 grouped_lines = [cleaned_lines[n:n+4] for n in range(0, len(cleaned_lines), 4)]
-trimmed_dict = {}
-trimmed_dict["genes"] = []
-trimmed_dict["exposures"] = []
-trimmed_dict["relationships"] = []
+trimmed_dict = {"genes": [], "exposures": [], "relationships": []}
 for group in grouped_lines:
     group.pop(0)
+# grouped_copy = grouped_lines.copy()
+# for sublist in grouped_copy:
+#     for elem in sublist:
+
 for group in grouped_lines:
-    gene, exposure, relation = None, None, None
+    # gene, exposure, relation = None, None, None
     # try:
     gene = group[0].split(":", 1)[1].strip()
     exposure = group[1].split(":", 1)[1].strip()
@@ -52,5 +65,16 @@ for group in grouped_lines:
     # except IndexError:
     #     continue
 
-# pprint.pprint(trimmed_dict)
+for key, value in trimmed_dict.copy().items():
+    for i, elem in enumerate(value):
+        # if elem.lower() in NULL_VALS:
+        if elem.lower() in (val.lower() for val in NULL_VALS):
+            for key, value in trimmed_dict.items():
+                del value[i]
+
+# for key, value in trimmed_dict.items():
+#     print(len(value))
+
+# triple_print(trimmed_dict)
+pprint.pprint(trimmed_dict)
 # print(trimmed_dict)
