7b1d020a35b2948381a7ffd16b6a9b475e741613
caufieldjh <j.harry.caufield@gmail.com>
2023-08-04 13:56:06 -0400

    Update gpt4all_test

--- tests/integration/test_knowledge_engines/test_gpt4all_engine.py
+++ tests/integration/test_knowledge_engines/test_gpt4all_engine.py
@@ -4,22 +4,16 @@ import unittest
 import yaml
 
 from ontogpt.engines.gpt4all_engine import GPT4AllEngine
-from ontogpt.templates.mendelian_disease import MendelianDisease
 from ontogpt.utils.model_utils import get_model
 
 TEMPLATE = "mendelian_disease.MendelianDisease"
 
-# This is a small test model
+# This is a small test model. It doesn't really
+# do extraction - it's a BERT model for sentence
+# embeddings - but it tests functionality.
 TEST_MODEL = "ggml-all-MiniLM-L6-v2-f16"
 
-PAPER = """
-Sly syndrome, also called mucopolysaccharidosis type VII (MPS-VII),
-is an autosomal recessive lysosomal storage disease caused by a deficiency
-of the enzyme β-glucuronidase. This enzyme is responsible for breaking down
-large sugar molecules called glycosaminoglycans (AKA GAGs, or mucopolysaccharides).
-The inability to break down GAGs leads to a buildup in many tissues and organs of the body.
-The severity of the disease can vary widely.
-"""
+TEXT_INPUT = "Peanut butter and cheese"
 
 class TestCore(unittest.TestCase):
     """Test annotation."""
@@ -31,19 +25,9 @@ class TestCore(unittest.TestCase):
     def test_extract(self):
         """Tests end to end knowledge extraction."""
         ke = self.ke
-        ann = ke.extract_from_text(PAPER)
+        ann = ke.extract_from_text(TEXT_INPUT)
         print(f"RESULTS={ann}")
         print(yaml.dump(ann.dict()))
         results = ann.extracted_object
-        if not isinstance(results, MendelianDisease):
-            raise ValueError(f"Expected MendelianDisease, got {type(results)}")
-
-    def test_extract_with_stub(self):
-        """Tests end to end knowledge extraction."""
-        ke = self.ke
-        ann = ke.extract_from_text(PAPER, object={"genes": ["HGNC:8850"]})  # PEX1
-        print(f"RESULTS={ann}")
-        print(yaml.dump(ann.dict()))
-        results = ann.extracted_object
-        if not isinstance(results, MendelianDisease):
-            raise ValueError(f"Expected MendelianDisease, got {type(results)}")
+        if results is not None:
+            raise ValueError(f"Unexpected output type, got {type(results)}")
