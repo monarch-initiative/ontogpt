f11f55126b44ac1ce2ade5cb9b4c7fe7f1ce8ee1
caufieldjh <j.harry.caufield@gmail.com>
2023-09-20 15:29:39 -0400

    Update OpenAI client unit tests to use gpt-3.5-turbo

--- tests/unit/test_clients/test_unit_openai_client.py
+++ tests/unit/test_clients/test_unit_openai_client.py
@@ -30,7 +30,7 @@ class TestCompletion(unittest.TestCase):
     def test_all_prompts(self):
         """Test all prompts."""
         prompt_doc = yaml.safe_load(open(PROMPTS_FILE))
-        default_engine = prompt_doc.get("default_engine", "gpt-3.5-turbo-instruct")
+        default_engine = prompt_doc.get("default_engine", "gpt-3.5-turbo")
         for prompt in prompt_doc["prompts"]:
             prompt_text = prompt["prompt"]
             if not isinstance(prompt_text, str):
