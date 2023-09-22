dc414853a3bcf9d32e970abb7d8a688b6dbde7a3
caufieldjh <j.harry.caufield@gmail.com>
2023-09-22 16:21:20 -0400

    Partial revert of changes to yaml_wrapper - there's an easier way

--- src/ontogpt/io/yaml_wrapper.py
+++ src/ontogpt/io/yaml_wrapper.py
@@ -43,17 +43,13 @@ def repr_str(dumper: RoundTripRepresenter, data: str):
 def dump_minimal_yaml(obj: Any, minimize=True, file: Optional[TextIO] = None) -> str:
     """Dump a YAML string, but eliminating Nones and empty lists and dicts."""
     yaml = YAML()
-    separator = YAML().load("")
     yaml.representer.add_representer(str, repr_str)
     yaml.default_flow_style = False
-    yaml.default_style=None
     yaml.indent(sequence=4, offset=2)
-    # A bit of a hack here to ensure all yaml output has a separator, even
-    # if it's a single document
     if not file:
         file = io.StringIO()
-        yaml.dump_all(documents=[separator, eliminate_empty(obj, not minimize)], stream=file)
+        yaml.dump(eliminate_empty(obj, not minimize), file)
         return file.getvalue()
     else:
-        yaml.dump_all(documents=[separator, eliminate_empty(obj, not minimize)], stream=file)
+        yaml.dump(eliminate_empty(obj, not minimize), file)
         return ""
