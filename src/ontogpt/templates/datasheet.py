852461789e54071e91763a6cd6d146f77d8794e7
caufieldjh <j.harry.caufield@gmail.com>
2023-10-18 16:04:34 -0400

    More fields for the datasheet schema

--- src/ontogpt/templates/datasheet.py
+++ src/ontogpt/templates/datasheet.py
@@ -58,10 +58,14 @@ class Dataset(NamedEntity):
     """
     name: Optional[str] = Field(None, description="""The name of the dataset.""")
     subsets: Optional[List[str]] = Field(default_factory=list, description="""Semicolon-separated list of names of any subsets of the dataset.""")
+    authors: Optional[List[str]] = Field(default_factory=list, description="""Semicolon-separated list of names of creators of the dataset.""")
     funding_organization: Optional[List[str]] = Field(default_factory=list, description="""Semicolon-separated list of organizations supporting creation of the dataset.""")
     funding_identifiers: Optional[List[str]] = Field(default_factory=list, description="""Semicolon-separated list of names, numbers, or other identifiers for grants supporting creation of the dataset.""")
     purpose: Optional[str] = Field(None, description="""The stated purpose of the dataset. This should be no more than 1 sentence.""")
+    limitations: Optional[str] = Field(None, description="""Known limitations of the dataset. This should be no more than 1 sentence.""")
+    warnings: Optional[str] = Field(None, description="""Warnings regarding content or application of the dataset.""")
     references: Optional[List[str]] = Field(default_factory=list, description="""Semicolon-separated list of publications describing the data or the process used to produce it.""")
+    license: Optional[str] = Field(None, description="""The set of rules defining the rights of the developer and the users of the dataset, for example, BSD-3 or CC0.""")
     id: str = Field(..., description="""A unique identifier for the named entity""")
     label: Optional[str] = Field(None, description="""The label (name) of the named thing""")
     
