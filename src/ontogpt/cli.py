f869dd48676c5510771beadff3f59bc63c9066e3
Zhenfeng Deng <zfdeng@boltzmann.ccbr.utoronto.ca>
2023-11-17 14:52:31 -0500

    fix typo

--- src/ontogpt/cli.py
+++ src/ontogpt/cli.py
@@ -1526,7 +1526,7 @@ def fill(model, template, object: str, examples, output, output_format, show_pro
 def openai_models(**kwargs):
     """List OpenAI models for prompt completion."""
     ai = OpenAIClient()
-    for model in ai.client.models.list()():
+    for model in ai.client.models.list():
         print(model)
 
 
