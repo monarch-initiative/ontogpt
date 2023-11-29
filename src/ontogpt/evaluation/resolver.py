3c67d4d16f8c9332ef6f373e082b7472bc896d7a
caufieldjh <j.harry.caufield@gmail.com>
2023-11-29 17:24:52 -0500

    Add EvalCRAFTConcepts to eval resolver

--- src/ontogpt/evaluation/resolver.py
+++ src/ontogpt/evaluation/resolver.py
@@ -3,12 +3,15 @@ from typing import Optional, Type, Union
 
 from class_resolver import ClassResolver
 
+from ontogpt.evaluation.craft.eval_craft_ner import EvalCRAFTConcepts
 from ontogpt.evaluation.ctd.eval_ctd import EvalCTD
 from ontogpt.evaluation.ctd.eval_ctd_ner import EvalCTDNER
 from ontogpt.evaluation.maxo.eval_maxo import EvalMAXO
 from ontogpt.evaluation.evaluation_engine import SPIRESEvaluationEngine
 
-resolver = ClassResolver([EvalCTD, EvalCTDNER, EvalMAXO], base=SPIRESEvaluationEngine)
+resolver = ClassResolver(
+    [EvalCRAFTConcepts, EvalCTD, EvalCTDNER, EvalMAXO], base=SPIRESEvaluationEngine
+)
 
 
 def create_evaluator(
