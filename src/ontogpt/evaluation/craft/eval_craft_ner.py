f3fb5f2a6a255fd859290d98558bebdb63883347
caufieldjh <j.harry.caufield@gmail.com>
2023-12-01 16:56:48 -0500

    Expanding eval

--- src/ontogpt/evaluation/craft/eval_craft_ner.py
+++ src/ontogpt/evaluation/craft/eval_craft_ner.py
@@ -79,6 +79,21 @@ TARGET_TYPES = {
     "Taxon": "NCBITaxon",
 }
 
+TARGET_ATTR_TYPES = [
+    "publication",
+    "anatomicalelements",
+    "biologicalprocesses",
+    "celltypes",
+    "cellularcomponents",
+    "chemicals",
+    "diseases",
+    "molecularfunctions",
+    "molecularprocesses",
+    "proteins",
+    "sequencefeatures",
+    "taxa",
+]
+
 RESULT_TYPES = [
     "true_positives",
     "num_true_positives",
@@ -111,6 +126,8 @@ class PredictionNER(BaseModel):
     named_entities: Optional[List[Any]] = None
 
 
+# TODO: expand to cover all entity types
+#       and adapt to the PredictionNER results form defined above
 class EvaluationObjectSetNER(BaseModel):
     """A result of performing named entity recognition."""
 
@@ -250,123 +267,123 @@ class EvalCRAFTConcepts(SPIRESEvaluationEngine):
     def eval(self) -> EvaluationObjectSetNER:
         """Evaluate the ability to extract and ground entities in CRAFT corpus."""
 
-        #     labeler = get_adapter("sqlite:obo:mesh")
-        #     if self.num_tests and isinstance(self.num_tests, int):
-        #         num_test = self.num_tests
-        #     else:
-        #         num_test = 1
-        #     ke = self.extractor
+        if self.num_tests and isinstance(self.num_tests, int):
+            num_test = self.num_tests
+        else:
+            num_test = 1
+        ke = self.extractor
+
         docs = list(self.load_test_cases())
 
-    #     shuffle(docs)
-    #     eos = EvaluationObjectSetNER(
-    #         test=docs[:num_test],
-    #         training=[],
-    #         predictions=[],
-    #     )
-    #     n = 1
-    #     for doc in eos.test:
-    #         logger.info(f"Iteration {n} of {num_test}")
-    #         n += 1
-    #         logger.info(doc)
-    #         text = f"Title: {doc.publication.title} Abstract: {doc.publication.abstract}"
-    #         pub = Publication.model_validate(
-    #             {
-    #                 "id": str(doc.publication.id),
-    #                 "title": doc.publication.title,
-    #                 "abstract": doc.publication.abstract,
-    #             }
-    #         )
-    #         predicted_obj = Document.model_validate(
-    #             {
-    #                 "publication": pub,
-    #                 "chemicals": [],
-    #                 "diseases": [],
-    #             }
-    #         )
-    #         named_entities: List[str] = []  # This stores the NEs for the whole document
-    #         ke.named_entities = []  # This stores the NEs the extractor knows about
+        shuffle(docs)
+        eos = EvaluationObjectSetNER(
+            test=docs[:num_test],
+            training=[],
+            predictions=[],
+        )
+        n = 1
+        for doc in eos.test:
+            logger.info(f"Iteration {n} of {num_test}")
+            n += 1
+            logger.info(doc.publication.title)
+            text = f"{doc.publication.title}\n{doc.publication.abstract}"
+            pub = Publication.model_validate(
+                {
+                    "id": str(doc.publication.id),
+                    "title": doc.publication.title,
+                    "abstract": doc.publication.abstract,
+                }
+            )
+            predicted_obj = Document.model_validate(
+                {
+                    "publication": pub,
+                    "anatomicalelements": [],
+                    "biologicalprocesses": [],
+                    "celltypes": [],
+                    "cellularcomponents": [],
+                    "chemicals": [],
+                    "diseases": [],
+                    "molecularfunctions": [],
+                    "molecularprocesses": [],
+                    "proteins": [],
+                    "sequencefeatures": [],
+                    "taxa": [],
+                }
+            )
+            named_entities: List[str] = []  # This stores the NEs for the whole document
+            ke.named_entities = []  # This stores the NEs the extractor knows about
 
-    #         if self.chunking:
-    #             text_list = chunk_text(text)
-    #         else:
-    #             text_list = iter([text])
+            if self.chunking:
+                text_list = chunk_text(text)
+            else:
+                text_list = iter([text])
 
-    #         for chunked_text in text_list:
-    #             extraction = ke.extract_from_text(chunked_text)
-    #             if extraction.extracted_object is not None:
-    #                 logger.info(
-    #                     f"{len(extraction.extracted_object.chemicals)}\
-    #                         chemical entities from window: {chunked_text}"
-    #                 )
-    #                 logger.info(
-    #                     f"{len(extraction.extracted_object.diseases)}\
-    #                         disease entities from window: {chunked_text}"
-    #                 )
-    #             if not predicted_obj and extraction.extracted_object is not None:
-    #                 predicted_obj = extraction.extracted_object
-    #             else:
-    #                 if predicted_obj is not None and extraction.extracted_object is not None:
-    #                     predicted_obj.chemicals.extend(extraction.extracted_object.chemicals)
-    #                     predicted_obj.diseases.extend(extraction.extracted_object.diseases)
-    #                     logger.info(
-    #                         f"{len(predicted_obj.chemicals)} chemical entities, after concatenation"
-    #                     )
-    #                     logger.info(
-    #                         f"{len(predicted_obj.diseases)} disease entities, after concatenation"
-    #                     )
-    #                     logger.debug(f"concatenated chemical entities: {predicted_obj.chemicals}")
-    #                     logger.debug(f"concatenated disease entities: {predicted_obj.diseases}")
-    #             if extraction.named_entities is not None:
-    #                 for entity in extraction.named_entities:
-    #                     if entity not in named_entities:
-    #                         named_entities.append(entity)
+            for chunked_text in text_list:
+                extraction = ke.extract_from_text(chunked_text)
+                if extraction.extracted_object is not None:
+                    logger.info("Found entities in active window.")
+                if not predicted_obj and extraction.extracted_object is not None:
+                    predicted_obj = extraction.extracted_object
+                else:
+                    if predicted_obj is not None and extraction.extracted_object is not None:
+                        for entity_type in TARGET_ATTR_TYPES:
+                            pred_entity_list_attr = getattr(predicted_obj, entity_type)
+                            extraction_list_attr = getattr(extraction.extracted_object, entity_type)
+                            new_entity_list = pred_entity_list_attr.extend(extraction_list_attr)
+                            setattr(predicted_obj, entity_type, new_entity_list)
+                            logger.info(f"{len(new_entity_list)} {entity_type} entities, after concatenation")
+                if extraction.named_entities is not None:
+                    for entity in extraction.named_entities:
+                        if entity not in named_entities:
+                            named_entities.append(entity)
 
-    #         def included(t: str):
-    #             if t.startswith("MESH:"):
-    #                 return t
+            # TODO: update based on known prefixes
+            def included(t: str):
+                if t.startswith("MESH:"):
+                    return t
 
-    #         predicted_obj.chemicals = [t for t in predicted_obj.chemicals if included(t)]
-    #         predicted_obj.diseases = [t for t in predicted_obj.diseases if included(t)]
+            # TODO: repeat for all entities, as above
+            predicted_obj.chemicals = [t for t in predicted_obj.chemicals if included(t)]
 
-    #         logger.info(f"{len(predicted_obj.chemicals)} filtered chemical entities (MESH only)")
-    #         logger.info(f"{len(predicted_obj.diseases)} filtered disease entities (MESH only)")
-    #         pred = PredictionNER(
-    #             predicted_object=predicted_obj, test_object=doc, named_entities=named_entities
-    #         )
-    #         named_entities.clear()
-    #         logger.info("PRED")
-    #         logger.info(yaml.dump(data=pred.model_dump()))
-    #         logger.info("Calc scores")
-    #         pred.calculate_scores(labelers=[labeler])
-    #         logger.info(yaml.dump(data=pred.model_dump()))
-    #         eos.predictions.append(pred)
-    #     self.calc_stats(eos)
-    #     return eos
+             # TODO: repeat for all entities, as above
+            logger.info(f"{len(predicted_obj.chemicals)} filtered entities")
+            pred = PredictionNER(
+                predicted_object=predicted_obj, test_object=doc, named_entities=named_entities
+            )
+            named_entities.clear()
+            logger.info("PRED")
+            logger.info(yaml.dump(data=pred.model_dump()))
+            logger.info("Calc scores")
+            pred.calculate_scores(labelers=[labeler])
+            logger.info(yaml.dump(data=pred.model_dump()))
+            eos.predictions.append(pred)
+        self.calc_stats(eos)
+        return eos
 
-    # def calc_stats(self, eos: EvaluationObjectSetNER):
-    #     num_true_positives_ce = sum(p.num_true_positives_ce for p in eos.predictions)
-    #     num_false_positives_ce = sum(p.num_false_positives_ce for p in eos.predictions)
-    #     num_false_negatives_ce = sum(p.num_false_negatives_ce for p in eos.predictions)
-    #     if num_true_positives_ce + num_false_positives_ce == 0:
-    #         logger.warning("No true positives or false positives for chemical entities.")
-    #         return
-    #     eos.precision_ce = num_true_positives_ce / (num_true_positives_ce + num_false_positives_ce)
-    #     eos.recall_ce = num_true_positives_ce / (num_true_positives_ce + num_false_negatives_ce)
-    #     if eos.precision_ce + eos.recall_ce == 0:
-    #         logger.warning("No precision or recall for chemical entities.")
-    #         return
-    #     eos.f1_ce = 2 * (eos.precision_ce * eos.recall_ce) / (eos.precision_ce + eos.recall_ce)
+    # TODO: adapt to aggregated form 
+    def calc_stats(self, eos: EvaluationObjectSetNER):
+        num_true_positives_ce = sum(p.num_true_positives_ce for p in eos.predictions)
+        num_false_positives_ce = sum(p.num_false_positives_ce for p in eos.predictions)
+        num_false_negatives_ce = sum(p.num_false_negatives_ce for p in eos.predictions)
+        if num_true_positives_ce + num_false_positives_ce == 0:
+            logger.warning("No true positives or false positives for chemical entities.")
+            return
+        eos.precision_ce = num_true_positives_ce / (num_true_positives_ce + num_false_positives_ce)
+        eos.recall_ce = num_true_positives_ce / (num_true_positives_ce + num_false_negatives_ce)
+        if eos.precision_ce + eos.recall_ce == 0:
+            logger.warning("No precision or recall for chemical entities.")
+            return
+        eos.f1_ce = 2 * (eos.precision_ce * eos.recall_ce) / (eos.precision_ce + eos.recall_ce)
 
-    #     num_true_positives_de = sum(p.num_true_positives_de for p in eos.predictions)
-    #     num_false_positives_de = sum(p.num_false_positives_de for p in eos.predictions)
-    #     num_false_negatives_de = sum(p.num_false_negatives_de for p in eos.predictions)
-    #     if num_true_positives_de + num_false_positives_de == 0:
-    #         logger.warning("No true positives or false positives for disease entities.")
-    #         return
-    #     eos.precision_de = num_true_positives_de / (num_true_positives_de + num_false_positives_de)
-    #     eos.recall_de = num_true_positives_de / (num_true_positives_de + num_false_negatives_de)
-    #     if eos.precision_de + eos.recall_de == 0:
-    #         logger.warning("No precision or recall for disease entities.")
-    #         return
-    #     eos.f1_de = 2 * (eos.precision_de * eos.recall_de) / (eos.precision_de + eos.recall_de)
+        num_true_positives_de = sum(p.num_true_positives_de for p in eos.predictions)
+        num_false_positives_de = sum(p.num_false_positives_de for p in eos.predictions)
+        num_false_negatives_de = sum(p.num_false_negatives_de for p in eos.predictions)
+        if num_true_positives_de + num_false_positives_de == 0:
+            logger.warning("No true positives or false positives for disease entities.")
+            return
+        eos.precision_de = num_true_positives_de / (num_true_positives_de + num_false_positives_de)
+        eos.recall_de = num_true_positives_de / (num_true_positives_de + num_false_negatives_de)
+        if eos.precision_de + eos.recall_de == 0:
+            logger.warning("No precision or recall for disease entities.")
+            return
+        eos.f1_de = 2 * (eos.precision_de * eos.recall_de) / (eos.precision_de + eos.recall_de)
