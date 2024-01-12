658805e3ec0b654a0b035627c5feae8ae2aa8479
caufieldjh <j.harry.caufield@gmail.com>
2024-01-12 15:42:01 -0500

    Deactivate HFHub client for now

--- src/ontogpt/clients/__init__.py
+++ src/ontogpt/clients/__init__.py
@@ -1,6 +1,6 @@
 """Clients for accessing different APIs."""
 from ontogpt.clients.openai_client import OpenAIClient  # noqa:F401
-from ontogpt.clients.hfhub_client import HFHubClient  # noqa:F401
+# from ontogpt.clients.hfhub_client import HFHubClient  # noqa:F401
 from ontogpt.clients.gpt4all_client import GPT4AllClient  # noqa:F401
 from ontogpt.clients.pubmed_client import PubmedClient  # noqa:F401
 from ontogpt.clients.soup_client import SoupClient  # noqa:F401
