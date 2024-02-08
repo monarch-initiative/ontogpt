3872b2c8068e58837a886966ac420d413f7035ee
caufieldjh <j.harry.caufield@gmail.com>
2024-02-07 22:32:06 -0500

    Updates to diagnostic_procedure

--- src/ontogpt/templates/diagnostic_procedure.py
+++ src/ontogpt/templates/diagnostic_procedure.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from datetime import datetime, date
 from enum import Enum
+
 from typing import List, Dict, Optional, Any, Union
 from pydantic import BaseModel as BaseModel, ConfigDict,  Field, field_validator
 import re
@@ -21,6 +22,7 @@ class ConfiguredBaseModel(BaseModel):
         extra = 'forbid',
         arbitrary_types_allowed=True,
         use_enum_values = True)
+    pass
 
 
 class NullDataOptions(str, Enum):
@@ -46,28 +48,28 @@ class ExtractionResult(ConfiguredBaseModel):
     extracted_object: Optional[Any] = Field(None, description="""The complex objects extracted from the text""")
     named_entities: Optional[List[Any]] = Field(default_factory=list, description="""Named entities extracted from the text""")
     
-        
+    
 
 class NamedEntity(ConfiguredBaseModel):
     
     id: str = Field(..., description="""A unique identifier for the named entity""")
     label: Optional[str] = Field(None, description="""The label (name) of the named thing""")
     
-        
+    
 
 class DiagnosticProcedure(NamedEntity):
     
     id: str = Field(..., description="""A unique identifier for the named entity""")
     label: Optional[str] = Field(None, description="""The label (name) of the named thing""")
     
-        
+    
 
 class Phenotype(NamedEntity):
     
     id: str = Field(..., description="""A unique identifier for the named entity""")
     label: Optional[str] = Field(None, description="""The label (name) of the named thing""")
     
-        
+    
 
 class ClinicalAttribute(NamedEntity):
     
@@ -75,27 +77,27 @@ class ClinicalAttribute(NamedEntity):
     id: str = Field(..., description="""A unique identifier for the named entity""")
     label: Optional[str] = Field(None, description="""The label (name) of the named thing""")
     
-        
+    
 
 class Quality(NamedEntity):
     
     id: str = Field(..., description="""A unique identifier for the named entity""")
     label: Optional[str] = Field(None, description="""The label (name) of the named thing""")
     
-        
+    
 
 class Unit(NamedEntity):
     
     id: str = Field(..., description="""A unique identifier for the named entity""")
     label: Optional[str] = Field(None, description="""The label (name) of the named thing""")
     
-        
+    
 
 class CompoundExpression(ConfiguredBaseModel):
     
     None
     
-        
+    
 
 class Triple(CompoundExpression):
     """
@@ -108,7 +110,7 @@ class Triple(CompoundExpression):
     subject_qualifier: Optional[str] = Field(None, description="""An optional qualifier or modifier for the subject of the statement, e.g. \"high dose\" or \"intravenously administered\"""")
     object_qualifier: Optional[str] = Field(None, description="""An optional qualifier or modifier for the object of the statement, e.g. \"severe\" or \"with additional complications\"""")
     
-        
+    
 
 class DiagnosticProceduretoPhenotypeAssociation(Triple):
     """
@@ -121,7 +123,7 @@ class DiagnosticProceduretoPhenotypeAssociation(Triple):
     subject_qualifier: Optional[str] = Field(None, description="""An optional qualifier or modifier for the procedure.""")
     object_qualifier: Optional[str] = Field(None, description="""An optional qualifier or modifier for the phenotype.""")
     
-        
+    
 
 class DiagnosticProceduretoAttributeAssociation(Triple):
     """
@@ -134,7 +136,7 @@ class DiagnosticProceduretoAttributeAssociation(Triple):
     subject_qualifier: Optional[str] = Field(None, description="""An optional qualifier or modifier for the procedure.""")
     object_qualifier: Optional[str] = Field(None, description="""An optional qualifier or modifier for the phenotype.""")
     
-        
+    
 
 class TextWithTriples(ConfiguredBaseModel):
     """
@@ -143,7 +145,7 @@ class TextWithTriples(ConfiguredBaseModel):
     publication: Optional[Publication] = Field(None)
     triples: Optional[List[Triple]] = Field(default_factory=list)
     
-        
+    
 
 class TextWithEntity(ConfiguredBaseModel):
     """
@@ -152,14 +154,14 @@ class TextWithEntity(ConfiguredBaseModel):
     publication: Optional[Publication] = Field(None)
     entities: Optional[List[str]] = Field(default_factory=list)
     
-        
+    
 
 class RelationshipType(NamedEntity):
     
     id: str = Field(..., description="""A unique identifier for the named entity""")
     label: Optional[str] = Field(None, description="""The label (name) of the named thing""")
     
-        
+    
 
 class ProcedureToPhenotypePredicate(RelationshipType):
     """
@@ -168,7 +170,7 @@ class ProcedureToPhenotypePredicate(RelationshipType):
     id: str = Field(..., description="""A unique identifier for the named entity""")
     label: Optional[str] = Field(None, description="""The label (name) of the named thing""")
     
-        
+    
 
 class ProcedureToAttributePredicate(RelationshipType):
     """
@@ -177,7 +179,7 @@ class ProcedureToAttributePredicate(RelationshipType):
     id: str = Field(..., description="""A unique identifier for the named entity""")
     label: Optional[str] = Field(None, description="""The label (name) of the named thing""")
     
-        
+    
 
 class Publication(ConfiguredBaseModel):
     
@@ -187,7 +189,7 @@ class Publication(ConfiguredBaseModel):
     combined_text: Optional[str] = Field(None)
     full_text: Optional[str] = Field(None, description="""The full text of the publication""")
     
-        
+    
 
 class AnnotatorResult(ConfiguredBaseModel):
     
@@ -195,7 +197,7 @@ class AnnotatorResult(ConfiguredBaseModel):
     object_id: Optional[str] = Field(None)
     object_text: Optional[str] = Field(None)
     
-        
+    
 
 
 # Model rebuild
