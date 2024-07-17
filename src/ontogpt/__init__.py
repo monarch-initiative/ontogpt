69c039ae08380842dc2e08d866aefd715082e4f2
caufieldjh <j.harry.caufield@gmail.com>
2024-07-17 13:05:53 -0400

    Add other half of getting model names

--- src/ontogpt/__init__.py
+++ src/ontogpt/__init__.py
@@ -3,10 +3,15 @@
 from importlib import metadata
 from pathlib import Path
 
+from litellm import get_model_cost_map
+
 # Define the default model
 rel_path = Path(__file__).resolve()
 
 DEFAULT_MODEL = "gpt-4o"
+
+# Build list of available models
+MODELS = get_model_cost_map("")
 
 try:
     __version__ = metadata.version(__name__)
